8754104196f83cad98abd6703433aaeadae17561
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-14 20:19:52 -0300

    Atualizações

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { useState } from "react";
 
-import Button from './components/Button';
-import ImageViewer from './components/ImageViewer';
+import Button from "./components/Button";
+import ImageViewer from "./components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 
-const PlaceholderImage = require('./assets/images/background-image.png')
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+   const [selectedImage, setSelectedImage] = useState(null);
+
+   const pickImageAsync = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+         allowsEditing: true,
+         quality: 1,
+      });
+
+      if (!result.canceled) {
+         setSelectedImage(result.assets[0].uri);
+      } else {
+         alert("You did not select any image.");
+      }
+   };
+
+   return (
+      <View style={styles.container}>
+         <View style={styles.imageContainer}>
+            <ImageViewer
+               placeholderImageSource={PlaceholderImage}
+               selectedImage={selectedImage}
+            />
+         </View>
+         <View style={styles.footerContainer}>
+            <Button
+               theme="primary"
+               label="Choose a photo"
+               onPress={pickImageAsync}
+            />
+            <Button label="Use this photo" />
+         </View>
+         <StatusBar style="auto" />
       </View>
-      <View style={styles.footerContainer}>
-      <Button label="Choose a photo" />
-      <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#25292e",
+      alignItems: "center",
+   },
 
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
+   imageContainer: {
+      flex: 1,
+      paddingTop: 58,
+   },
 });
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,15 +1,17 @@
 import { StyleSheet, Image } from "react-native";
 
-export default function ImageViewer ({ placeholderImageSource }) {
-    return (
-        <Image source={placeholderImageSource} style={StyleSheet.image} />
-    );
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+   const imageSource = selectedImage
+      ? { uri: selectedImage }
+      : placeholderImageSource;
+
+   return <Image source={placeholderImageSource} style={StyleSheet.image} />;
 }
 
-const styles = StyleSheet.create ({
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    },
+const styles = StyleSheet.create({
+   image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+   },
 });
